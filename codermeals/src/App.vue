0619cf06abe2bd42d299720608748b823b7226e4
alejoquintana <alejoquintana00@gmail.com>
2022-09-05 22:23:24 -0300

    clase 5

---
+++ codermeals/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app" class="fcc">
+    <Titulo />
+		<ProductsCard class="mxw"
+			v-for="product in products"
+			:key="product.id"
+			:product="product"
+		/>
+  </div>
+</template>
+
+<script>
+import Titulo from './components/Titulo.vue'
+import ProductsCard from './components/ProductsCard.vue'
+
+export default {
+	name: 'App',
+	components:{
+		Titulo,ProductsCard
+	},
+	data(){
+		return{
+			products:[
+				{id:1,name:"termo",color:"verde"},
+				{id:2,name:"mate",color:"rojo"},
+				{id:3,name:"bombilla",color:"azul"},
+				{id:4,name:"vaso",color:"transparente"},
+			]
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.mxw{
+	max-width: 200px;
+}
+.fcc{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+</style>
---
+++ codermeals/src/components/ProductsCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="margin border">
+    <h3>{{product.name}}</h3>
+    <img src="../assets/No-image-available.png">
+    <div class="d-flex justify-content-between">
+        <span>#{{product.id}}</span>
+        <span>{{product.color}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        product: Object
+    }
+}
+</script>
+
+<style>
+.margin{
+    margin: 18px 0;
+}
+.border{
+    border: 2px solid red;
+    border-radius: 24px;
+}
+</style>
